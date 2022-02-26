181fc8e098c468c5356d419e72b32614f20f1fc9
hyeonukdev <lego7407@gmail.com>
2022-02-26 17:20:48 +0900

    :pencil2: Fix typos

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -1,15 +1,15 @@
-const { merge } = require("webpack-merge")
-const config = require("../webpack.config.js")
+const { merge } = require('webpack-merge')
+const config = require('../webpack.config.js')
 
 module.exports = merge(config, {
-  mode: "development",
-  devtool: "eval-cheap-source-map",
+  mode: 'development',
+  devtool: 'eval-cheap-source-map',
   devServer: {
-    contentBase: "../dist",
-    host: "localhost",
+    contentBase: '../dist',
+    host: 'localhost',
     port: 9000,
     open: true,
     hot: true,
-    historyApiFallback: true,
-  },
+    historyApiFallback: true
+  }
 })
--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,55 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
   entry: {
-    app: "./src/index.tsx",
+    app: './src/index.tsx'
   },
   optimization: {
     splitChunks: {
       cacheGroups: {
         vendor: {
-          chunks: "initial",
-          name: "vendor",
-          enforce: true,
-        },
-      },
-    },
+          chunks: 'initial',
+          name: 'vendor',
+          enforce: true
+        }
+      }
+    }
   },
   resolve: {
-    modules: [path.join(__dirname, "src"), "node_modules"],
-    extensions: [".js", ".tsx", ".ts"],
+    modules: [path.join(__dirname, 'src'), 'node_modules'],
+    extensions: ['.js', '.tsx', '.ts'],
     alias: {
-      "@": path.resolve(__dirname, "src/"),
-    },
+      '@': path.resolve(__dirname, 'src/')
+    }
   },
   output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "[name].[chunkhash].js",
-    chunkFilename: "[name].[chunkhash].chunk.js",
+    path: path.join(__dirname, '/dist'),
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[name].[chunkhash].chunk.js'
   },
   module: {
     rules: [
       {
         test: /\.ts$|tsx/,
         exclude: /node_module/,
-        use: ["babel-loader", "eslint-loader"],
+        use: ['babel-loader', 'eslint-loader']
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-    ],
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
+      template: './public/index.html'
     }),
     new MiniCssExtractPlugin({
-      filename: "style.css",
+      filename: 'style.css'
     }),
-    new CleanWebpackPlugin(),
-  ],
-};
+    new CleanWebpackPlugin()
+  ]
+}
