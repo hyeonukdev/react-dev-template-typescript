6c6c15d10aab2d287c1775e52d67ce9645663461
hyeonukdev <lego7407@gmail.com>
2022-02-26 17:25:19 +0900

    :lipstick: Update Detail page

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,15 +1,7 @@
-import { Switch } from 'antd'
-
-function onChange(checked: any) {
-  console.log(`switch to ${checked}`)
-}
-
 const Detail = () => {
   return (
     <>
-      <div>123</div>
-      <Switch defaultChecked onChange={onChange} />
-      123
+      <h1>Detail Page</h1>
     </>
   )
 }
